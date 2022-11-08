4856da62bb0cb04eaeed4349e9bba8d21d70d3e7
Mujahidul Amin <mujahidulamin1108152@gmail.com>
2022-11-09 00:15:45 +0600

    environment-variable-added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAHDLJGd7M6za9ftf7Hi2bsmxYYKGQfms4",
-  authDomain: "wildlife-photography-5ef21.firebaseapp.com",
-  projectId: "wildlife-photography-5ef21",
-  storageBucket: "wildlife-photography-5ef21.appspot.com",
-  messagingSenderId: "852608157991",
-  appId: "1:852608157991:web:21ec16c37eb8c45f98c102"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
